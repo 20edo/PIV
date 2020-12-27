4ca0c9b26cf5056bdce83608e24f7289034b45cc
20edo <20edoardo@gmail.com>
2020-12-27 13:26:18 +0100

    Built function to catch flashed images, removed non-flashed average from images and saved number of images averaged. TODO: substitute with a random noise the flash region, equalize with rank local filters the images and run meanField with the improved images

---
+++ Scripts/flash_mask.py
@@ -0,0 +1,40 @@
+def flash_mask(image, bin_treshold=250, region_connectivity=2, area_treshold=100):
+    '''Returns a mask with true where the image is flashed
+    *****************************INPUTS
+    bin_treshold        -> Threshold for binarization
+    region_connectivity -> Value of connectivity used to look for regions (1    or 2)
+    area_trshold        -> Minimum area required to consider a region flashed
+    *****************************OUTPUTS
+    mask                -> 1 where the image is flashed
+    number              -> Number of flashed regions'''
+    import numpy as np
+    from skimage import morphology
+    import skimage.measure as measure
+    import skimage.segmentation as segmentation
+    import skimage as sk
+    import pandas as pd
+    
+    # Image binarization
+    binarization = image > bin_treshold
+    binarization = binarization.astype('uint8')
+
+    # Calculate otsu treshold
+    tresh = sk.filters.threshold_otsu(image)
+
+    # Morphological closing ( holes inside a flashed region mean nothing)
+    bw = sk.morphology.closing(frame_a > tresh, sk.morphology.square(3))
+    
+    # Split the image in regions
+    labeled_image = sk.measure.label(bw,connectivity=region_connectivity)
+    
+    # Inizialize mask 
+    mask = np.zeros_like(image)
+    number = 0
+    # Turn to 1 positions where image is flashed
+    for region in sk.measure.regionprops(labeled_image):
+        if region.area >= area_treshold:
+            min_row, min_col, max_row, max_col = region.bbox
+            mask[min_row:max_row,min_col:max_col] = region.filled_image
+            number +=1
+    
+    return mask, number
